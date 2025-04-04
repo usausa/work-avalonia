d2648c232e17ec954279080e63109c5fd3004579
usausa <machi.pon@gmail.com>
2025-04-04 09:29:48 +0900

    Add

---
+++ Screen/AvaloniaApp/Views/MainPageViewModel.cs
@@ -0,0 +1,6 @@
+namespace AvaloniaApp.Views;
+
+public partial class MainPageViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Screen/AvaloniaApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+namespace AvaloniaApp.Views;
+
+using Avalonia.Controls;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
