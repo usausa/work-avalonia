459b375734510b3ae77530585d62dec89370a7ef
usausa <machi.pon@gmail.com>
2025-04-06 16:10:23 +0900

    Add generic host

--- Work/AvaloniaApp/App.axaml.cs
+++ Work/AvaloniaApp/App.axaml.cs
@@ -7,27 +7,43 @@ using Avalonia.Markup.Xaml;
 
 using AvaloniaApp.Views;
 
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+// ReSharper disable once PartialTypeWithSinglePart
 public partial class App : Application
 {
+    private IHost host = default!;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+        host = CreateHost();
     }
 
-    public override void OnFrameworkInitializationCompleted()
+    // ReSharper disable once AsyncVoidMethod
+    public override async void OnFrameworkInitializationCompleted()
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             // Avoid duplicate validations from both Avalonia and the CommunityToolkit.
             // More info: https://docs.avaloniaui.net/docs/guides/development-guides/data-validation#manage-validationplugins
             DisableAvaloniaDataAnnotationValidation();
-            desktop.MainWindow = new MainWindow
+
+            desktop.Exit += async (_, _) =>
             {
-                DataContext = new MainPageViewModel()
+                await host.StopAsync(TimeSpan.FromSeconds(5));
+                host.Dispose();
             };
+
+            // TODO
+            desktop.MainWindow = host.Services.GetRequiredService<MainWindow>();
         }
 
         base.OnFrameworkInitializationCompleted();
+
+        await host.StartAsync();
     }
 
     private static void DisableAvaloniaDataAnnotationValidation()
@@ -40,5 +56,15 @@ public partial class App : Application
         {
             BindingPlugins.DataValidators.Remove(plugin);
         }
+    }
+
+    private static IHost CreateHost()
+    {
+        var builder = Host.CreateApplicationBuilder();
+
+        // TODO
+        builder.Services.AddSingleton<MainWindow>();
+
+        return builder.Build();
     }
 }
--- Work/AvaloniaApp/Views/MainWindow.axaml.cs
+++ Work/AvaloniaApp/Views/MainWindow.axaml.cs
@@ -7,5 +7,10 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+#if !DEBUG
+        Position = new Avalonia.PixelPoint(0, 0);
+        WindowState = WindowState.FullScreen;
+#endif
     }
 }
