e7e7c79e553c025480452bde55b02e65434ad3c2
usausa <machi.pon@gmail.com>
2025-04-08 14:42:41 +0900

    Update

--- EmbeddedControl/WorkApp/Views/MainWindowViewModel.cs
+++ EmbeddedControl/WorkApp/Views/MainWindowViewModel.cs
@@ -9,7 +9,7 @@ using CommunityToolkit.Mvvm.ComponentModel;
 public sealed partial class MainWindowViewModel : ViewModelBase, IDisposable
 {
     private const double AccelVelocity = 32d / 60;
-    private const double BreakVelocity = 48d / 60;
+    private const double BrakeVelocity = 48d / 60;
     private const double DefaultVelocity = 20d / 60;
 
     private readonly PeriodicTimer timer;
@@ -20,6 +20,12 @@ public sealed partial class MainWindowViewModel : ViewModelBase, IDisposable
 
     [ObservableProperty]
     public partial int Speed { get; set; }
+
+    [ObservableProperty]
+    public partial bool Accel { get; set; }
+
+    [ObservableProperty]
+    public partial bool Brake { get; set; }
 
     public MainWindowViewModel()
     {
@@ -46,16 +52,21 @@ public sealed partial class MainWindowViewModel : ViewModelBase, IDisposable
             var fps = 0;
             var speed = 0d;
             var prevSpeed = 0;
+            var prevAccel = false;
+            var prevBrake = false;
 
             var watch = Stopwatch.StartNew();
             while (await timer.WaitForNextTickAsync(cancellationTokenSource.Token))
             {
                 // Speed
-                if (KeyPad.Instance.BrakePressed)
+                var brake = KeyPad.Instance.BrakePressed;
+                var accel = KeyPad.Instance.AccelPressed;
+
+                if (brake)
                 {
-                    speed = Math.Max(0, speed - BreakVelocity);
+                    speed = Math.Max(0, speed - BrakeVelocity);
                 }
-                else if (KeyPad.Instance.AccelPressed)
+                else if (accel)
                 {
                     speed = Math.Min(255, speed + AccelVelocity);
                 }
@@ -65,14 +76,20 @@ public sealed partial class MainWindowViewModel : ViewModelBase, IDisposable
                 }
 
                 var currentSpeed = (int)speed;
-                if (currentSpeed != prevSpeed)
+                if ((currentSpeed != prevSpeed) ||
+                    (accel != prevAccel) ||
+                    (brake != prevBrake))
                 {
                     await Dispatcher.UIThread.InvokeAsync(() =>
                     {
                         Speed = currentSpeed;
+                        Accel = accel;
+                        Brake = brake;
                     });
 
                     prevSpeed = currentSpeed;
+                    prevAccel = accel;
+                    prevBrake = brake;
                 }
 
                 // FPS
