bd493e103a2cce46063a5f17f33ddf9fa1558229
usausa <machi.pon@gmail.com>
2025-04-08 13:53:37 +0900

    Update

--- Platform/AvaloniaApp/Views/MainWindow.axaml.cs
+++ Platform/AvaloniaApp/Views/MainWindow.axaml.cs
@@ -1,18 +1,9 @@
 namespace AvaloniaApp.Views;
 
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Media;
-
-using static System.Net.Mime.MediaTypeNames;
 
 public partial class MainWindow : Window
 {
-    public static readonly StyledProperty<Brush> BackgroundProperty =
-        AvaloniaProperty.Register<Control, Brush>(
-            nameof(Background),
-            defaultValue: Brushes.Transparent);
-
     public MainWindow()
     {
         InitializeComponent();
